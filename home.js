d7dcd1dd68c04518fcfcb45d0b653ae2c2f85caa
kilix <shauryabhalla.fr@gmail.com>
2025-07-28 23:46:58 +0530

    updated hero section

--- home.js
+++ home.js
@@ -8,13 +8,40 @@ document.addEventListener("DOMContentLoaded", function () {
             loading.classList.add("fade-out");
             
             setTimeout(function () {
-                const navhr = document.querySelector("#navhr");
                 const hero = document.querySelector("#hero");
                 const items = document.querySelector("#items");
                 hero.classList.add("fade-up");
                 items.classList.add("fade-up");
-                navhr.classList.add("expand");
             }, 800);
         }, 500);
     }, 1500);
 });
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
